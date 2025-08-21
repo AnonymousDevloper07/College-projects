c6689bacc7bfb3f062842af71fcb10b037b4e93e
AnonymousDevloper07 <oggyandthecockroaches2007@gmail.com>
2025-08-21 12:01:55 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    printf("Give me a number: ");
+    scanf("%d", &x);
+
+    if (x % 7 == 0) {
+        printf("%d is divisible by 7.\n", x);
+    } else {
+        printf("%d is not divisible by 7.\n", x);
+    }
+
+    return 0;
+}
